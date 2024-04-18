1f2a0fce26d4811272bafeb6221b9c3a16e3a8d1
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-04-18 10:02:25 -0300

    ajustes busca videos

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,10 +1,18 @@
 import { conectaApi } from "./conectaAPI.js";
+import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
     evento.preventDefault();
 
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const lista = document.querySelector("[data-lista]")
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
+
+    while (lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 
     console.log(busca)
 }
